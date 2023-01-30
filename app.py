ad19c8eeda435b64340e12d718d2de83c96056a3
Caique Matos <43851217+cmatos17@users.noreply.github.com>
2023-01-30 14:54:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -64,9 +64,10 @@ if input_pdf is not None:
         @st.cache
         def convert_df(df):
         # IMPORTANT: Cache the conversion to prevent computation on every rerun    
-            return df.to_csv().encode('utf-8')
+            return df.to_csv(index=False).encode('utf-8')
         
         csv_file = convert_df(table[int(option)].df)
+        
         st.download_button(
             label="Download",
             data=csv_file,
